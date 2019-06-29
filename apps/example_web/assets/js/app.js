17e869f634290ac6ab3eb95a88ec843eb07399c3
ne_Sachirou <utakata.c4se@gmail.com>
2019-06-30 06:07:42 +0900

    React

--- apps/example_web/assets/js/app.js
+++ apps/example_web/assets/js/app.js
@@ -15,3 +15,9 @@ import "phoenix_html";
 //
 // Local files can be imported directly using relative paths, for example:
 import socket from "./socket";
+
+import main from "./components.jsx";
+
+window.addEventListener("DOMContentLoaded", () => {
+  main();
+});
---
+++ apps/example_web/assets/js/components.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App(props) {
+  return <div>App</div>;
+}
+
+export default function main() {
+  ReactDOM.render(<App />, document.getElementById("app"));
+}
